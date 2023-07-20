256e7169c11662e5305c9291fbdc10d3b00ceb38
Hocko <chengkuan689@163.com>
2023-07-21 00:33:52 +0800

    chore: 修改默认用户信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://raw.githubusercontent.com/hocko333/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'November Rain',
+      description: 'Star on <a href="https://github.com/hocko333/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
